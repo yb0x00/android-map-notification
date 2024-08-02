775ff4d7fbc51a2206c1e0f710c7a653fa5970cb
yb0x00 <yeongbeen0x00@gmail.com>
2024-08-02 12:04:43 +0900

    feat: Add notification permission request
    - 알림 권한 설정

--- app/src/main/java/campus/tech/kakao/map/view/SplashActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/view/SplashActivity.kt
@@ -1,10 +1,20 @@
 package campus.tech.kakao.map.view
 
+import android.app.AlertDialog
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.provider.Settings
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import campus.tech.kakao.map.R
@@ -23,9 +33,8 @@ class SplashActivity : AppCompatActivity() {
 
         setBinding()
         setViewModel()
-        splashViewModel.fetchServiceState()
-        setScreen()
 
+        askNotificationPermission()
     }
 
     private fun setBinding() {
@@ -35,6 +44,11 @@ class SplashActivity : AppCompatActivity() {
     private fun setViewModel() {
         binding.lifecycleOwner = this
         binding.viewModel = splashViewModel
+    }
+
+    private fun startAppFlow() {
+        splashViewModel.fetchServiceState()
+        setScreen()
     }
 
     private fun setScreen() {
@@ -48,5 +62,71 @@ class SplashActivity : AppCompatActivity() {
     private fun navigateToHomeMapActivity() {
         val intent = Intent(this, HomeMapActivity::class.java)
         startActivity(intent)
+        finish()
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            startAppFlow()
+        } else {
+            showNotificationPermissionDialog()
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // 33 (TIRAMISU) 버전 이상
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.POST_NOTIFICATIONS
+                ) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                startAppFlow()
+            } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                showNotificationPermissionDialog()
+            } else {
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
+    private fun showNotificationPermissionDialog() {
+        AlertDialog.Builder(this@SplashActivity).apply {
+            setTitle(getString(R.string.ask_notification_permission_dialog_title))
+            setMessage(
+                String.format(
+                    "\n권한을 허용하시면, 다양한 알림 소식을 받으실 수 있습니다.\n(알림에서 %s의 알림 권한을 허용해주세요.)",
+                    getString(R.string.app_name)
+                )
+            )
+            setPositiveButton(getString(R.string.accept_notification_permission)) { _, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }
+            setNegativeButton(getString(R.string.deny_notification_permission)) { _, _ ->
+                handleAppExit()
+            }
+            setCancelable(true)
+
+            setOnCancelListener {
+                handleAppExit()
+            }
+            show()
+        }
+    }
+
+    private fun handleAppExit() {
+        Toast.makeText(
+            this@SplashActivity, "앱을 사용하기 위해 알림 권한이 필요합니다. 권한을 허용해주세요!",
+            Toast.LENGTH_SHORT
+        ).show()
+        Handler(Looper.getMainLooper()).postDelayed({
+            finish()
+        }, 1000)
     }
 }
