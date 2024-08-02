ac1e9d525e74e8241344dd5f19792db3b228b11a
yb0x00 <yeongbeen0x00@gmail.com>
2024-08-02 14:41:24 +0900

    feat: Add custom notification for foreground mode
    - 앱이 포그라운드 상태일 경우 커스텀 Notification을 발생

--- app/src/main/java/campus/tech/kakao/map/data/firebase/FirebaseMessagingService.kt
+++ app/src/main/java/campus/tech/kakao/map/data/firebase/FirebaseMessagingService.kt
@@ -1,17 +1,57 @@
 package campus.tech.kakao.map.data.firebase
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import campus.tech.kakao.map.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
-class FirebaseMessagingService : FirebaseMessagingService(){
-
+class FirebaseMessagingService : FirebaseMessagingService() {
+    private lateinit var notificationManager: NotificationManager
     override fun onMessageReceived(message: RemoteMessage) {
-        Log.d("yeong","From: ${message.from}")
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        message.notification?.let {
-            Log.d("yeong","Message Notification Title: ${it.title}")
-            Log.d("yeong","Message Notification Body: ${it.body}")
+        createNotificationChannel()
+        val builder = NotificationCompat.Builder(
+            this,
+            CHANNEL_ID
+        )
+            .setSmallIcon(R.drawable.map_navigation_icon)
+            .setContentTitle(getString(R.string.myNotification_title))
+            .setContentText(getString(R.string.myNotification_body))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText("앱이 실행 중일 때는 포그라운드 알림이 발생합니다.")
+            )
+            .setAutoCancel(true)
+
+        notificationManager.notify(NOTIFICATION_ID, builder.build())
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d("yeong", "New Token: $token")
+    }
+
+    companion object {
+        private const val NOTIFICATION_ID = 111111
+        private const val CHANNEL_ID = "main_default_channel"
+        private const val CHANNEL_NAME = "지도앱 알림"
+    }
+
+    private fun createNotificationChannel() {
+        val descriptionText = getString(R.string.fcm_channel_description)
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_DEFAULT
+        ).apply {
+            description = descriptionText
         }
+        notificationManager.createNotificationChannel(channel)
     }
 }
